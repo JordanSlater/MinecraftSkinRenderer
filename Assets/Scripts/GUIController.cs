9059e6632e39eb77c32c50ab6741869fc4dfdd71
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2021-02-17 23:53:42 -0500

    Added link to help page

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -11,50 +11,69 @@ public class GUIController : MonoBehaviour
     
     public RotationController rotationController;
 
+    // gui elements that shouldn't be clicked on while rotating
     List<Rect> guiElements = new List<Rect>();
+    private const int buffer = 10;
+    public Texture helpTexture;
+    private Rect filePathFieldRect;
+    private Rect filePathGhostTextRect;
+    private Rect rotationSensitivityLabelRect;
+    private Rect rotationSensitivityRect;
+    private Rect alexToggleRect;
+    private const int helpSize = 30;
+    private string helpURL = "https://github.com/JordanSlater/MinecraftSkinRenderer";
+    private Rect helpRect;
+    private GUIStyle helpStyle;
+    const int statusMeassageRectHeight = 200;
+    private Rect statusMeassageRect;
+    private GUIStyle statusMessageStyle;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Application.targetFrameRate = 30;
         skinLoader = transform.root.GetComponent<SkinLoader>();
+
+        filePathFieldRect = new Rect(buffer, buffer, Screen.width - 2*buffer, 20);
+        guiElements.Add(filePathFieldRect);
+        filePathGhostTextRect = new Rect(buffer + 5, filePathFieldRect.y, filePathFieldRect.width, filePathFieldRect.height);
+        guiElements.Add(filePathGhostTextRect);
+
+        rotationSensitivityLabelRect = new Rect(buffer, guiElements.Last().yMax, 100, 20);
+        guiElements.Add(rotationSensitivityLabelRect);
+        rotationSensitivityRect = new Rect(rotationSensitivityLabelRect.xMax + buffer, rotationSensitivityLabelRect.yMin + 5, 150, 20);
+        guiElements.Add(rotationSensitivityRect);
+
+        alexToggleRect = new Rect(buffer, guiElements.Last().yMax - 5, 120, 15);
+        guiElements.Add(alexToggleRect);
+
+        helpRect = new Rect(Screen.width - helpSize - buffer, Screen.height - helpSize - buffer, helpSize, helpSize);
+        helpStyle = new GUIStyle() { imagePosition = ImagePosition.ImageOnly };
+        guiElements.Add(helpRect);
+
+        statusMeassageRect = new Rect(buffer, Screen.height - statusMeassageRectHeight - buffer, helpRect.xMin - 2*buffer, statusMeassageRectHeight);
     }
 
     void OnGUI()
     {
-        const int buffer = 10;
-        guiElements.Clear();
+        GUI.skin.label.alignment = TextAnchor.LowerLeft;
 
         string prevFilePathField = filePathField;
-        var filePathFieldRect = new Rect(buffer, buffer, Screen.width - 2*buffer, 20);
-        guiElements.Add(filePathFieldRect);
         filePathField = GUI.TextField(filePathFieldRect, filePathField);
         if (System.String.IsNullOrEmpty(filePathField))
-        {
-            var filePathGhostTextRect = new Rect(buffer + 5, filePathFieldRect.y, filePathFieldRect.width, filePathFieldRect.height);
-            guiElements.Add(filePathGhostTextRect);
             GUI.Label(filePathGhostTextRect, "Type path to file here");
-        }
         if (prevFilePathField != filePathField)
             skinLoader.SetFilePath(filePathField);
 
-        var rotationSensitivityLabelRect = new Rect(buffer, guiElements.Last().yMax, 100, 20);
-        guiElements.Add(rotationSensitivityLabelRect);
         GUI.Label(rotationSensitivityLabelRect, "Look sensitivity: ");
-        var rotationSensitivityRect = new Rect(rotationSensitivityLabelRect.xMax + buffer, rotationSensitivityLabelRect.yMin + 5, 150, 20);
-        guiElements.Add(rotationSensitivityRect);
+        
         rotationController.sensitivity = GUI.HorizontalSlider(rotationSensitivityRect, rotationController.sensitivity, 1, 10);
         
-        var AlexToggleRect = new Rect(buffer, guiElements.Last().yMax - 5, 120, 15);
-        guiElements.Add(AlexToggleRect);
-        skinLoader.UseAlexVariant = GUI.Toggle(AlexToggleRect, skinLoader.UseAlexVariant, "Use Alex Model");
+        skinLoader.UseAlexVariant = GUI.Toggle(alexToggleRect, skinLoader.UseAlexVariant, "Use Alex Model");
 
-        var prevAlignment = GUI.skin.label.alignment;
-        GUI.skin.label.alignment = TextAnchor.LowerLeft;
-        const int statusMeassageRectHeight = 200;
-        var statusMeassageRect = new Rect(buffer, Screen.height - statusMeassageRectHeight - buffer, Screen.width - 2*buffer, statusMeassageRectHeight);
+        if (GUI.Button(helpRect, helpTexture, helpStyle))
+            Application.OpenURL(helpURL);
+        
         GUI.Label(statusMeassageRect, skinLoader.StatusMessage);
-        GUI.skin.label.alignment = prevAlignment;
     }
 
     // Update is called once per frame
